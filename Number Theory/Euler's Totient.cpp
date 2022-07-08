5924464ffd37b5f19183a50de9b84393bcff6a12
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2022-07-09 01:37:09 +0530

    Create Euler's Totient.cpp

---
+++ Number Theory/Euler's Totient.cpp
@@ -0,0 +1,18 @@
+//Time Complexity: O(sqrt(n) log(n))
+
+typedef ll long long;
+// count of numbers from 1 to n that are co-prime with n.
+ll eulertotient(ll n)
+{
+    ll result = n;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            result -= result / i;
+        while (n % i == 0)
+            n /= i;
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
