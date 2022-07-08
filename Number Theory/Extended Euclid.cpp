10db10365f4876e801db00cfa0192c320a6b2461
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2022-07-09 01:28:04 +0530

    Create Extended Euclid.cpp

---
+++ Number Theory/Extended Euclid.cpp
@@ -0,0 +1,20 @@
+//TIme Complexity: O(log(max(a,b)))
+
+typedef long long ll;
+ll d, x, y;
+void extendedEuclid(ll a, ll b)
+{
+    if (b == 0)
+    {
+        d = a;
+        x = 1;
+        y = 0;
+    }
+    else
+    {
+        extendedEuclid(b, a % b);
+        ll temp = x;
+        x = y;
+        y = temp - (a / b) * y;
+    }
+}
