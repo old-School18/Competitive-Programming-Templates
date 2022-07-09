a4a1baf0164d8827c1d13a0e9cb1fadd3a34ff87
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2022-07-09 09:26:12 +0530

    Update Segmented Sieve of Eratosthenes Large n.cpp

--- Number Theory/Segmented Sieve of Eratosthenes Large n.cpp
+++ Number Theory/Segmented Sieve of Eratosthenes Large n.cpp
@@ -1,7 +1,6 @@
 // Time Complexity: O(sqrt(n))
 // Determine prime numbers from 1 to n if n is large.
 
-typedef long long ll;
 void sieve(ll n, vector<ll> &prime)
 {
     bool is_prime[n + 1];
@@ -37,29 +36,29 @@ vector<ll> segmented_sieve_large_n(ll n)
     ll limit = floor(sqrt(n)) + 1ll;
     vector<ll> prime;
     sieve(limit, prime);
-    vector<ll> primr = prime;
+    vector<ll> prime_till_n = prime;
+    bool is_prime[limit + 1];
     ll lo = limit, hi = 2ll * limit;
     while (lo < n)
     {
         if (hi >= n)
             hi = n;
-        map<ll, bool> is_prime;
+        memset(is_prime, true, sizeof(is_prime));
+
         for (ll i = 0; i < (ll) prime.size(); i++)
         {
             ll lo_lim = floor(lo / prime[i]) * prime[i];
             if (lo_lim < lo)
                 lo_lim += prime[i];
-            if (lo_lim == prime[i])
-                lo_lim += prime[i];
             for (ll j = lo_lim; j <= hi; j += prime[i])
-                is_prime[j] = true;
+                is_prime[j - lo] = false;
         }
         for (ll i = lo; i <= hi; i++)
-            if (is_prime[i] == false)
-                primr.push_back(i);
+            if (is_prime[i - lo] == true)
+                prime_till_n.push_back(i);
 
         lo += limit;
         hi += limit;
     }
-    return primr;
+    return prime_till_n;
 }
