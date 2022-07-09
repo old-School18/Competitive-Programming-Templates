6e453fc7d38d4eb05a2a879e1f10820353e9aa1c
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2022-07-09 10:21:41 +0530

    Create Prime Factorize.cpp

---
+++ Number Theory/Prime Factorize.cpp
@@ -0,0 +1,23 @@
+// Time Complexity: O(sqrt(n)log n)
+
+typedef long long ll;
+set<ll> prime_factor(ll n)
+{
+    set<ll> pf;
+    while (n % 2 == 0)
+    {
+        pf.insert(2);
+        n /= 2;
+    }
+    for (ll x = 3; x * x <= n; x += 2)
+    {
+        while (n % x == 0)
+        {
+            pf.insert(x);
+            n /= x;
+        }
+    }
+    if (n > 1)
+        pf.insert(n);
+    return pf;
+}
