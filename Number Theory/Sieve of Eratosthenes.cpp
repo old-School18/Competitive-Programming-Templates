2aa84ecfc74c138e27d5b9a5397cad70b04e4556
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2022-07-09 08:59:16 +0530

    Create Sieve of Eratosthenes.cpp

---
+++ Number Theory/Sieve of Eratosthenes.cpp
@@ -0,0 +1,25 @@
+// Time Complexity: O(n log log n)
+// Determine prime numbers from 1 to n.
+
+void sieve(ll n)
+{
+    bool isPrime[n + 1];
+    for (ll i = 0; i <= n; i++)
+    {
+        if (i % 2 != 0)
+            isPrime[i] = true;
+        else
+            isPrime[i] = false;
+    }
+    isPrime[1] = false;
+    isPrime[2] = true;
+
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (isPrime[i] == true)
+        {
+            for (ll j = i * i; j <= n; j += 2 * i)
+                isPrime[j] = false;
+        }
+    }
+}
