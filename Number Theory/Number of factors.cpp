c26f524a96f806bd5e3929abbb6fa4feba115234
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2022-07-09 10:23:23 +0530

    Update Number of factors.cpp

--- Number Theory/Number of factors.cpp
+++ Number Theory/Number of factors.cpp
@@ -4,7 +4,12 @@ typedef long long ll;
 ll prime_factors[n + 1];
 void prime_factorize(ll n)
 {
-    for (ll i = 2; i * i <= n; i++)
+    while(n % 2 == 0)
+    {
+        prime_factors[2]++;
+        n /= 2;
+    }
+    for (ll i = 3; i * i <= n; i+=2)
     {
         while (n % i == 0)
         {
