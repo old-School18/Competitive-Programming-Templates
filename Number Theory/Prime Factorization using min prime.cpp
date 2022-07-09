b29d9487c67bd9f6a50e45c39a287b0003390991
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2022-07-09 11:03:39 +0530

    Create Prime Factorization using min prime.cpp

---
+++ Number Theory/Prime Factorization using min prime.cpp
@@ -0,0 +1,42 @@
+// Time Complexity: O(n log log n)
+
+typedef long long ll;
+vector<ll> find_min_primes(ll n)
+{
+    vector<ll> min_prime(n + 1, 0ll);
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (min_prime[i] == 0)
+        {
+            // if i is prime
+            for (ll j = i * i; j <= n; j += i)
+            {
+                if (min_prime[j] == 0)
+                {
+                    min_prime[j] = i;
+                }
+            }
+        }
+    }
+
+    for (ll i = 2; i <= n; ++i)
+    {
+        if (min_prime[i] == 0)
+        {
+            min_prime[i] = i;
+        }
+    }
+    return min_prime;
+}
+
+set<ll> factorize(ll n)
+{
+    set<ll> pf;
+    vector<ll> min_prime = find_min_primes(n);
+    while (n != 1)
+    {
+        pf.insert(min_prime[n]);
+        n /= min_prime[n];
+    }
+    return pf;
+}
