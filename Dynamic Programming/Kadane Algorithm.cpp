73ce41a118cd3ff4939a8bbdf015ded96cd280ec
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2022-09-05 20:11:48 +0530

    Update Kadane Algorithm.cpp

--- Dynamic Programming/Kadane Algorithm.cpp
+++ Dynamic Programming/Kadane Algorithm.cpp
@@ -1,6 +1,8 @@
 // Time Complexity: O(n)
 // Maximum subarray sum
 
+1. Without dp:
+
 typedef ll long long;
 ll kadane(ll a[], ll n)
 {
@@ -21,4 +23,22 @@ ll kadane(ll a[], ll n)
         ll k = *z;
         return k;
     }
+}
+
+
+2. With dp:
+
+typedef long long ll;
+ll kadane_with_dp(ll a[], ll n)
+{
+    ll dp[n] = {0};
+    dp[0] = a[0];
+    for (ll i = 0; i < n; i++)
+    {
+        dp[i] = max(dp[i - 1] + a[i], a[i]);
+    }
+    ll max_overall = LLONG_MIN;
+    for (ll i = 0; i < n; i++)
+        max_overall = max(max_overall, dp[i]);
+    return max_overall;
 }
