23e804d8bcdd96b5e3c10bac54393e2809cd03fc
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2022-09-05 20:38:55 +0530

    Create Longest Increasing Subsequence.cpp

---
+++ Dynamic Programming/Longest Increasing Subsequence.cpp
@@ -0,0 +1,20 @@
+// Time Compleixty: O(n^2)
+
+ll lis(ll a[], ll n)
+{
+    ll lis[n];
+    memset(lis, 0, sizeof(lis));
+    for (ll i = 0; i < n; i++)
+    {
+        lis[i] = 1;
+        for (ll j = 0; j < i; j++)
+        {
+            if (a[j] < a[i] && lis[j] + 1 > [i])
+                lis[i] = lis[j] + 1;
+        }
+    }
+    ll ans = 0ll;
+    for (ll i = 0; i < n; i++)
+        ans = max(ans, lis[i]);
+    return ans;
+}
