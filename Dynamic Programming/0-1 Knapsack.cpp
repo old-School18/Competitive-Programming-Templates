1816eb33a6ab2cd25f202ef36c7dacbf6c7d17a2
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2022-09-09 15:59:53 +0530

    Update 0-1 Knapsack.cpp

--- Dynamic Programming/0-1 Knapsack.cpp
+++ Dynamic Programming/0-1 Knapsack.cpp
@@ -1,6 +1,6 @@
 // Time Complexity: O(n*W)
 // Fill a knapsack with given weights such that the sum of values of the chosen weights is maximum. A weight can be
-// added only once.
+// added only once. The sum of the chosen weights is less than or equal to the capacity of the knapsack W. 
 
 
 // 2D DP
@@ -8,15 +8,21 @@
 typedef long long ll;
 ll zerone_knapsack(ll w[], ll v[], ll n, ll W)
 {
-    ll dp[n][W + 1];
-    memset(dp, -1, sizeof(dp));
-    for (ll i = 0; i < n; i++)
-        for (ll j = w[i]; j <= W; j++)
-            if (i == 0)
-                dp[i][j] = max(dp[i][j], v[i]);
-            else if (dp[i - 1][j - w[i]] != -1)
-                dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i]);
-    return dp[n-1][W];
+    ll dp[n + 1][W + 1];
+    memset(dp, 0, sizeof(dp));
+    for (ll i = 0; i <= n; i++)
+        for (ll j = 0; j <= W; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= w[i])
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i]);
+            }
+        }
+    return dp[n - 1][W];
 }
 
 // 1D DP Style A
@@ -26,15 +32,21 @@ ll zerone_knapsack(ll w[], ll v[], ll n, ll W)
 {
     ll dp[2][W + 1];
     ll flag = 0;
-    memset(dp, -1, sizeof(dp));
+    memset(dp, 0, sizeof(dp));
     for (ll i = 0; i < n; i++)
     {
         flag = 1 - flag;
-        for (ll j = w[i]; j <= W; j++)
-            if (i == 0)
-                dp[flag][j] = max(dp[flag][j], v[i]);
-            else if (dp[1 - flag][j - w[i]] != -1)
-                dp[flag][j] = max(dp[flag][j], dp[1 - flag][j - w[i]] + v[i]);
+        for (ll j = 0; j <= W; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[flag][j] = 0;
+            else
+            {
+                dp[flag][j] = dp[1 - flag][j];
+                if (j >= w[i])
+                    dp[flag][j] = max(dp[flag][j], dp[1 - flag][j - w[i]] + v[i]);
+            }
+        }
     }
     return dp[flag][W];
 }
@@ -45,11 +57,10 @@ typedef long long ll;
 ll zerone_knapsack(ll w[], ll v[], ll n, ll w)
 {
     ll dp[n + 1];
-    memset(dp, -1, sizeof(dp));
+    memset(dp, 0, sizeof(dp));
     dp[0] = 0;
     for (ll i = 0; i < n; i++)
         for (ll j = W; j >= w[i]; j--)
-            if (dp[j - w[i]] != -1)
-                dp[j] = dp[j - w[i]] + v[i];
+            dp[j] = dp[j - w[i]] + v[i];
     return dp[W];
 }
