6fe2f16a0e8858c9c8e6892a20213acc57807e33
Shyam Prasad <44469629+old-School18@users.noreply.github.com>
2022-09-09 16:05:13 +0530

    Update Integer Knapsack.cpp

--- Dynamic Programming/Integer Knapsack.cpp
+++ Dynamic Programming/Integer Knapsack.cpp
@@ -1,15 +1,17 @@
 // Time Complexity: O(W*n)
-// Fill a knapsack with given weights such that the sum of values of the chosen weights is maximum. A weight can be added more than once.
+// Fill a knapsack with given weights such that the sum of values of the chosen weights is maximum. A weight can be
+// added more than once. The sum of the chosen weights is less than or equal to the capacity of the knapsack W.
 
 typedef long long ll;
 ll integer_knapsack(ll w[], ll v[], ll n, ll W)
 {
-    ll dp[W + 1] = {0};
-    memset(dp, -1, sizeof(dp));
-    dp[0] = 0;
+    ll dp[W + 1];
+    memset(dp, 0, sizeof(dp));
     for (ll i = 0; i <= W; i++)
         for (ll j = 0; j < n; j++)
-            if (i >= w[j] && dp[i – w[j]] != -1)
+        {
+            if (i >= w[j])
                 dp[i] = max(dp[i], dp[i - w[j]] + v[j]);
+        }
     return dp[W];
 }
